9c02e3e6e08a864643f49c99d2ea76d5545cd390
fig781 <adeneilers@gmail.com>
2021-03-16 21:59:04 -0700

    Parses commands into struct

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"net"
+	"strings"
 )
 
 const (
@@ -12,7 +14,14 @@ const (
 	connType = "tcp"
 )
 
+type Command struct {
+	verb  string
+	key   string
+	value string
+}
+
 func main() {
+
 	//Listen for connections
 	dStream, err := net.Listen(connType, connHost + ":" + connPort)
 	if err != nil {
@@ -36,13 +45,43 @@ func main() {
 
 //Handles data that streams in from TCP server connection
 func handleConnection(conn net.Conn) {
+	fmt.Println("Ran")
 	for {
 		data, err := bufio.NewReader(conn).ReadString('\n')
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		fmt.Print(data)
+
+		parsedCommand, err := parseCommand(data)
+		if err != nil{
+			fmt.Println(err)
+		}
+		fmt.Print(parsedCommand)
+
 	}
 	//conn.Close()
 }
+
+func parseCommand(rawData string) (Command, error) {
+	parsedData := strings.Split(rawData, " ")
+	fmt.Printf("len=%d %v\n", len(parsedData), parsedData)
+
+	if len(parsedData) == 2 {
+		return Command {
+			verb: parsedData[0],
+			key: parsedData[1],
+		}, nil
+	} else if len(parsedData) == 3 {
+		return Command {
+			verb: parsedData[0],
+			key: parsedData[1],
+			value: parsedData[2],
+		}, nil
+	} else {
+		return Command{}, errors.New("invalid command format")
+	}
+}
+
+//Need to parse the string input to understand what to do
+//based on the parsed string, run an action
